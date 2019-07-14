b3110553b5b625604dc9bddf6963f9bda0a002ac
Kyle Krick <krkrick@gmail.com>
2019-07-14 16:59:58 -0700

    added static typing & try catch, updated comments

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Azure.Services.AppAuthentication;
 
 namespace KeyVault
@@ -9,18 +10,24 @@ namespace KeyVault
     {
         static void Main(string[] args)
         {
-            // Replace vaultName with the name of your Azure Key Vault
-            var yourKeyVault = "vaultName";
+            // Replace myKeyURI with a URI from your Azure Key Vault
+            string myKeyURI = "";
 
             // Connect using client credentials to access Key Vault
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
 
-            // Get keys from KeyVault -- note returns <IPage<KeyItem>>
-            // To-Do catch exception for bad key vault URL
-            keyVaultClient.GetKeysAsync($"https://{yourKeyVault}.vault.azure.net/").GetAwaiter().GetResult();
-
-            // To-Do Actually use a key for something...
+            // Get key from Key Vault
+            try
+            {
+                KeyBundle key = keyVaultClient.GetKeyAsync(myKeyURI).GetAwaiter().GetResult();
+                // To-Do Actually use a key for something...
+                Console.WriteLine(key.Key);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
