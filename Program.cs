9cb0566a411aef135c2084a92774d0e04f9cabf8
Kyle Krick <krkrick@gmail.com>
2019-07-14 17:41:11 -0700

    moved keyURI to appsettings using config and config builder

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace KeyVault
 {
@@ -10,8 +12,10 @@ namespace KeyVault
     {
         static void Main(string[] args)
         {
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("C:/Users/k_r_k/OneDrive/Documents/Programming/AZ203/KeyVault/appsettings.json");
+            IConfiguration config = builder.Build();
             // Replace myKeyURI with a URI from your Azure Key Vault
-            string myKeyURI = "";
+            string myKeyURI = config.GetSection("myKey").Value;
 
             // Connect using client credentials to access Key Vault
             AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
